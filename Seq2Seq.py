0a63cab300b3d65cb8205a25267f7090fecf3dce
1042813577 <1042813577@qq.com>
2020-05-15 15:24:29 +0800

    subtle change

--- Seq2Seq.py
+++ Seq2Seq.py
@@ -7,7 +7,10 @@ from Component import Attention
 def load_word2vec(path):
     with open(path, 'r', encoding='utf-8') as f:
         word2vec = {}
-        for word, vec in f.readlines():
+        for line in f:
+            temp = line.strip().split('\t')
+            word = temp[0]
+            vec = temp[1:]
             word2vec[word] = vec
     return word2vec
 
